f3300114b9a5e70d88a554916f86f16133adf75a
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-01-27 18:19:26 +0100

    Final changes

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { text } from 'stream/consumers'
 
-export const Square = (props: { onClick: () => void, text: string}) => {
+export const Square = (props: { onClick: () => void, text: string }) => {
+    const changeButtonClass = () => {
+        if (props.text === 'X') {
+            return 'x'
+        } else if (props.text === 'O') {
+            return 'o'
+        }
+    }
 
     return (
         <button
-            className='square'
+            className={`square ${changeButtonClass()}`}
             onClick={props.onClick}
         >
             {props.text}
